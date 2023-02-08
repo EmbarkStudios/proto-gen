48ec95a0c117f3c5526c6a11e0e1d9b16fb92ba6
Tom Solberg <me@sbg.dev>
2023-02-08 20:34:21 +0100

    add `--generate-transport` flag.

--- proto-gen-cli/src/main.rs
+++ proto-gen-cli/src/main.rs
@@ -39,6 +39,9 @@ struct TonicOpts {
     /// Whether to build client code
     #[clap(short = 'c', long)]
     build_client: bool,
+
+    #[clap(long)]
+    generate_transport: bool,
     /// Type attributes to add.
     #[clap(long = "type-attribute", value_parser=KvValueParser)]
     type_attributes: Vec<(String, String)>,
@@ -110,7 +113,8 @@ fn main() -> Result<(), i32> {
     let opts: Opts = Opts::parse();
     let mut bldr = tonic_build::configure()
         .build_client(opts.tonic_opts.build_client)
-        .build_server(opts.tonic_opts.build_server);
+        .build_server(opts.tonic_opts.build_server)
+        .build_transport(opts.tonic_opts.generate_transport);
 
     for (k, v) in opts.tonic_opts.type_attributes.into_iter() {
         bldr = bldr.type_attribute(k, v);
