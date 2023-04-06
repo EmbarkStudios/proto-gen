4dabe601868b7e21f00ba304de94c3937cb9916f
MarcusGrass <marcus.grass@protonmail.com>
2023-04-06 15:39:32 +0200

    Remove unused import

--- proto-gen/src/main.rs
+++ proto-gen/src/main.rs
@@ -165,7 +165,6 @@ fn run_ws(opts: WorkspaceOpts, bldr: Builder, commit: bool, format: bool) -> Res
 #[cfg(all(test, feature = "protoc-tests"))]
 mod tests {
     use super::*;
-    use std::io::ErrorKind;
     use std::path::Path;
     use tempfile::TempDir;
 
